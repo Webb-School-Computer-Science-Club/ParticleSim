fccac7de68f4c82e588e997f080332461bea11a0
Dmitri K <69704069+DtPeach3707@users.noreply.github.com>
2021-10-31 12:40:18 -0400

    Added dark mode toggle and docstring

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+"""
+main.py
+The main menu of the GUI
+"""
 from SimCases.DistDisp import DistDisp
 import pygame
 
@@ -9,29 +13,42 @@ dark_mode = False
 pygame.init()
 window = pygame.display.set_mode((width, height))
 clock = pygame.time.Clock()
-if dark_mode:
-    background = (0, 0, 0)
-    main_color = (255, 255, 255)
-    rect_color = (100, 100, 100)
-else:
-    background = (255, 255, 255)
-    main_color = (0, 0, 0)
-    rect_color = (150, 150, 150)
+
+
+def change_palette(dark_mode): # Changing dark mode colors
+    if dark_mode:
+        background = (0, 0, 0)
+        main_color = (255, 255, 255)
+        rect_color = (100, 100, 100)
+    else:
+        background = (255, 255, 255)
+        main_color = (0, 0, 0)
+        rect_color = (150, 150, 150)
+    return background, main_color, rect_color
+
+
+background, main_color, rect_color = change_palette(dark_mode)
 window.fill(background)
 pygame.display.set_caption('Point-particle Simulation Menu')
 
 # Making loop function for main window
 
 
-def loop():
+def loop(dark_mode):
     crashed = False
+    background, main_color, rect_color = change_palette(dark_mode)
     while not crashed:
         for event in pygame.event.get():  # Event handling
             if event.type == pygame.QUIT:
                 crashed = True
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 if 150 <= pygame.mouse.get_pos()[0] <= 450 and 150 <= pygame.mouse.get_pos()[1]:
-                    new_sim = DistDisp()
+                    new_sim = DistDisp(width=width, height=height, dark_mode=dark_mode)
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_d:  # Dark mode toggle
+                    dark_mode = not dark_mode
+                    background, main_color, rect_color = change_palette(dark_mode)
+                    window.fill(background)                   
         font = pygame.font.SysFont('freesans', 20)
         pygame.draw.rect(window, background, (150, 150, 300, 100))
         text = font.render('Distance-Displacement', False, main_color)
@@ -47,7 +64,7 @@ def loop():
 
 
 try:
-    loop()
+    loop(dark_mode)
 except KeyboardInterrupt:
     print('Thank you for using this GUI!')
     
